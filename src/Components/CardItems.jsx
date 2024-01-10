230ad2fc7b2b1b869f28bdced1d946d4432309e4
Steve <Stevekivaamwanzia@gmail.com>
2024-01-10 22:22:43 +0300

    env fix

--- src/Components/CardItems.jsx
+++ src/Components/CardItems.jsx
@@ -8,7 +8,10 @@ import {format,parseISO} from 'date-fns'
 
 
 const CardItems = ({githubUser}) => {
+    const formatedDate = githubUser?.created_at ? format(parseISO(githubUser?.created_at), 'd MMM yyyy') : "Not available";
   return (
+    
+
     <div className='flex lg:[45%] md:[60%] w-full flex-col bg-white dark:bg-[#1E2A47]  rounded p-6 shadow' >
         <div className='flex gap-5 w-full'>
         <span className='w-[20%]'>
@@ -20,12 +23,13 @@ const CardItems = ({githubUser}) => {
             <div className='w-full flex gap-2 justify-between items-start'>
                 <div className='flex flex-col gap-1'>
                     <h1 className='text-[24px] text-black dark:text-[#FFFFFF] font-bold font-spaceMono m-0 p-0'>
-                        {githubUser?.login === null ? "The Octocat": githubUser?.login}
+                        {githubUser?.login ? githubUser?.login : "The Octocat" }
                     </h1>
                     <h4 className='text-[14px] text-primaryBlue font-spaceMono m-0 p-0'>@{githubUser?.login}</h4>
                 </div>
                 <h4 className='text-[13px] text-gray-100 m-0 font-spaceMono mt-2'>
 
+                    Joined {formatedDate}
                     {/* Joined {format(parseISO(githubUser?.created_at),'d MMM yyyy')} */}
                 {/* Joined {githubUser?.created_at} */}
                 </h4>
@@ -34,7 +38,7 @@ const CardItems = ({githubUser}) => {
         <div className='justify-end flex'>
             <p className='text-gray-200 dark:text-[#FFFFFF] text-[13px] font-spaceMono font-normal w-[80%] p-0 m-0'>
                 { 
-                githubUser?.bio === null ? "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros.": githubUser?.bio   
+                githubUser?.bio ? githubUser?.bio : "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros."   
                 }   
             </p>
         </div>
@@ -43,42 +47,42 @@ const CardItems = ({githubUser}) => {
                 <div className='flex flex-col justify-center items-center'>
                     <h4 className='text-[13px] text-gray-200 dark:text-[#FFFFFF]'>Repo</h4>
                     <p className='font-bold font-spaceMono text-[15px] text-black dark:text-[#FFFFFF]'>
-                        {githubUser?.public_repos === null ? '0' : githubUser?.public_repos}</p>
+                        {githubUser?.public_repos ? githubUser?.public_repos : '0' }</p>
                 </div>
                 <div className='flex flex-col justify-center items-center'>
                     <h4 className='text-[13px] text-gray-200 dark:text-[#FFFFFF] '>Following</h4>
                     <p className='font-bold font-spaceMono text-[15px] text-black dark:text-[#FFFFFF]'>
-                        {githubUser?.following === null ? '0' : githubUser?.following}</p>
+                        {githubUser?.following ? githubUser?.following : '0'}</p>
                 </div>
                 <div className='flex flex-col justify-center items-center'>
                     <h4 className='text-[13px] text-gray-200 dark:text-[#FFFFFF]'>Followers</h4>
                     <p className='font-bold font-spaceMono text-[15px] text-black dark:text-[#FFFFFF]'>
-                        {githubUser?.followers === null ? '0' : githubUser?.followers}</p>
+                        {githubUser?.followers ? githubUser?.followers : '0'}</p>
                 </div>
             </div>
             <div className='grid md:grid-cols-2 gap-4 grid-cols-1 mt-5'>
                 <div className='flex gap-2 items-center'>
                     <span><img src={mapIcon} alt="" /></span>
                     <p className='text-gray-200 dark:text-[#FFFFFF] text-bold font-spaceMono text-[13px]'>
-                        {githubUser?.location === null ? 'Not available' : githubUser?.location }</p>
+                        {githubUser?.location ? githubUser?.location : 'Not available' }</p>
                 </div>
                 <div className='flex gap-2 items-center'>
                     <span><img src={twitter} alt="" /></span>
                     <p className='text-gray-200 dark:text-[#FFFFFF] text-bold font-spaceMono text-[13px]'>
                         {
-                            githubUser?.twitter_username === null ? 'Not available' : githubUser?.twitter_username
+                            githubUser?.twitter_username ? githubUser?.twitter_username  : 'Not available' 
                         }
                     </p>
                 </div>
                 <div className='flex gap-2 items-center'>
                     <span><img src={linkIcon} alt="" /></span>
                     <p className='text-gray-200 dark:text-[#FFFFFF] text-bold font-spaceMono text-[13px]'>
-                        {githubUser?.blog === "" ? 'Not available' : githubUser?.blog}</p>
+                        {githubUser?.blog ? githubUser?.blog : 'Not available' }</p>
                 </div>
                 <div className='flex gap-2 items-center'>
                     <span><img src={office} alt="" /></span>
                     <p className='text-gray-200 dark:text-[#FFFFFF] text-bold font-spaceMono text-[13px]'>
-                        @{githubUser?.company}</p>
+                        @{githubUser?.company ? githubUser?.company : 'Not available' }</p>
                 </div>
             </div>
         </div>
